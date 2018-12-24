32d41ce2373ffadf8b604468f86293c968898434
DanielDenenberg <dannydenenberg@gmail.com>
2018-12-23 22:56:43 -0600

    changed to ES6

--- matrix.js
+++ matrix.js
@@ -1,36 +1,63 @@
-// var m = new Matrix(2,3);
+// let m = new Matrix(2,3);
+// to see an array in the console use `console.table(my_array)`
+function table(mat) {
+  console.table(mat.matrix);
+}
 
 // create a new matrix
-function Matrix(rows, cols) {
-  this.rows = rows;
-  this.cols = cols;
-  this.matrix = [];
+class Matrix {
+  constructor(rows, cols) {
+    this.rows = rows;
+    this.cols = cols;
+    this.matrix = [];
 
-  // initialize all of the values to zero
-  for (var i = 0; i < this.rows; i++) {
-    this.matrix[i] = [];
-    for (var j = 0; j < this.cols; j++) {
-      this.matrix[i][j] = 0;
+    // initialize all of the values to zero
+    for (let i = 0; i < this.rows; i++) {
+      this.matrix[i] = [];
+      for (let j = 0; j < this.cols; j++) {
+        this.matrix[i][j] = 0;
+      }
     }
   }
-}
 
 
-// scalarly multiply a number by a matrix
-// a method of all matrix classes
-Matrix.prototype.multiply = function(n) {
-  for (var i = 0; i < this.rows; i++) {
-    for (var j = 0; j < this.cols; j++) {
-      this.matrix[i][j] *= n;
+  randomize() {
+    // initialize all of the values to random values
+    for (let i = 0; i < this.rows; i++) {
+      for (let j = 0; j < this.cols; j++) {
+        this.matrix[i][j] = Math.floor(Math.random() * 10);
+      }
     }
   }
-}
 
-// scale a number by a matrix
-Matrix.prototype.add = function(n) {
-  for (var i = 0; i < this.rows; i++) {
-    for (var j = 0; j < this.cols; j++) {
-      this.matrix[i][j] += n;
+
+  // multiply: scalar, element-wize, or matrix product (dot product)
+  // scale a number by a matrix or multiply matricies element wize
+  multiply(n) {
+    for (let i = 0; i < this.rows; i++) {
+      for (let j = 0; j < this.cols; j++) {
+        this.matrix[i][j] *= n;
+      }
     }
+  }
+
+  // scale a number by a matrix or add matricies element wize
+  add(n) {
+    // check if we need to add element wize (if we are adding matricies)
+    if (n instanceof Matrix) {
+      for (let i = 0; i < this.rows; i++) {
+        for (let j = 0; j < this.cols; j++) {
+          this.matrix[i][j] += n.matrix[i][j];
+        }
+      }
+      // otherwize
+    } else {
+      for (let i = 0; i < this.rows; i++) {
+        for (let j = 0; j < this.cols; j++) {
+          this.matrix[i][j] += n;
+        }
+      }
+    }
+
   }
 }
